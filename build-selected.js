8c577259d958f691735b6477932c72aee02cd42b
Your Name <you@example.com>
2024-04-11 23:36:51 +0800

    1

--- build-selected.js
+++ build-selected.js
@@ -31,46 +31,22 @@ const keys = [
 ];
 for (let i = 1; i <= 1231692; ++i) {
     if (i%1000 == 0) console.log(i);
-    const content = fs.readFileSync(
-        path.join('json', Math.floor(i/1000).toString(), i.toString() + '.html')
-    ).toString();
-
-    const ytitle = find(content, '<P class=ytitle><B>', '</B>');
-    const mtitle = find(content, '<P class=mtitle><B>', '</B>');
-    const ftitle = find(content, '<P class=ftitle><B>', '</B>');
-    const authors = find(content, '<P class=author>', '</P>');
+    const json = JSON.parse(fs.readFileSync(
+        path.join('json', Math.floor(i/1000).toString(), i.toString() + '.json')
+    ).toString());
 
     if (!(
-        includes(ytitle, keys) || 
-        includes(mtitle, keys) || 
-        includes(ftitle, keys) || includes(authors, [
+        includes(json.ytitle, keys) || 
+        includes(json.mtitle, keys) || 
+        includes(json.ftitle, keys) || includes(authors, [
             '江青',
             '王洪文',
             '张春桥',
             '姚文元',
         ])
     )) continue;
-    const text = find(content, '<P class=text>', '</P>');
-    const date_str = find(content, '<FONT class=brief>日期:</FONT></TD>\r\n<TD class=td1>', '</TD>');
-    const res = {
-        id: i,
-        date: [{year: parseInt(date_str.split('.')[0]), month: parseInt(date_str.split('.')[1]), day: parseInt(date_str.split('.')[2])}],
-        page: find(content, '<TD class=td>版次:</TD>\r\n<TD class=td1>', '</TD>'),
-        source: find(content, '<TD class=td vAlign=top>来源:</TD>\r\n<TD class=td1>', '</TD>'),
-        region: find(content, '<TD class=td vAlign=top>地区:</TD>\r\n<TD class=td1>', '</TD>'),
-        masthead: find(content, '<TD class=td vAlign=top>版名:</TD>\r\n<TD class=td1>', '</TD>'),
-        special_issue: find(content, '<TD class=td vAlign=top>专刊:</TD>\r\n<TD class=td1>', '</TD>'),
-        column: find(content, '<TD class=td vAlign=top>栏目:</TD>\r\n<TD class=td1>', '</TD>'),
-        genre: find(content, '<TD class=td vAlign=top>体裁:</TD>\r\n<TD class=td1>', '</TD>'),
-        count: find(content, '<TD class=td>字数:</TD>\r\n<TD class=td1>', '</TD>'),
-        ytitle: ytitle,
-        mtitle: mtitle,
-        ftitle,
-        authors: authors.split(';').map(i => i.trim()).filter(i => i),
-        text: text.split('<BR>').map(i => ({type: 'paragraph', text: i.trim()})).filter(i => i.text),
-    }
     const dir = path.join(__dirname, 'selected', Math.floor(i / 1000).toString());
     fs.ensureDirSync(dir);
-    fs.writeFileSync(path.join(dir, i + '.json'), JSON.stringify(res))
+    fs.writeFileSync(path.join(dir, i + '.json'), JSON.stringify(json))
 }
 console.log('done');
