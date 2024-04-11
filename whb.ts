8cfd1434b6dd9af8fc6d49e4b13e10309eea8dbb
Your Name <you@example.com>
2024-04-11 20:09:37 +0800

    whb

---
+++ whb.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "whb",
+      "name": "文汇报",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "",
+      "files": []
+    },
+    "parser_option": {},
+    "parser_id": "whb",
+    "path": "selected",
+    "resource_type": "book",
+    "version": 2
+  }
